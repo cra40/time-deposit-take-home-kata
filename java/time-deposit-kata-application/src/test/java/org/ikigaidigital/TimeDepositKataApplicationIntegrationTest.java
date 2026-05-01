0134127603ef67b946a282b307375828195b15fc
Chaithra Ramachandran <chaithraram.study@gmail.com>
2026-05-01 23:50:01 +0100

    Adding integration tests

--- java/time-deposit-kata-application/src/test/java/org/ikigaidigital/TimeDepositKataApplicationIntegrationTest.java
+++ java/time-deposit-kata-application/src/test/java/org/ikigaidigital/TimeDepositKataApplicationIntegrationTest.java
@@ -4,20 +4,23 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(
         locations = "classpath:application-test.properties")
-class TimeDepositKataApplicationTest {
+class TimeDepositKataApplicationIntegrationTest {
 
     private static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>("postgres:11")
             .withDatabaseName("testdatabase")
@@ -39,8 +42,17 @@ class TimeDepositKataApplicationTest {
         CONTAINER.stop();
     }
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void applicationContextLoads() {
-        assertTrue(true);
+    void getTimeDeposits() {
+        String expected = """
+                {"id":1,"planType":"BASIC","balance":1000.00,"days":30,"withdrawals":[{"id":1,"timeDepositId":1,"amount":100.00,"date":"2020-01-01T00:00:00.000+00:00"},{"id":2,"timeDepositId":1,"amount":100.00,"date":"2020-01-02T00:00:00.000+00:00"}]},{"id":2,"planType":"STUDENT","balance":1000.00,"days":45,"withdrawals":[]},{"id":3,"planType":"PREMIUM","balance":1000.00,"days":30,"withdrawals":[]}""";
+        String actual = restTemplate.getForObject("http://localhost:" + port + "/time-deposits", String.class);
+        assertThat(actual).contains(expected);
     }
 }
